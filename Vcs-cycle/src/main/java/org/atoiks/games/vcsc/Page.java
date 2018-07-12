b341fed7e42c73b24c285eed6ca586eadfb31f3b
Paul Teng <plankp@outlook.com>
2018-07-12 15:03:17 -0400

    Further pull up methods

--- Vcs-cycle/src/main/java/org/atoiks/games/vcsc/Page.java
+++ Vcs-cycle/src/main/java/org/atoiks/games/vcsc/Page.java
@@ -35,6 +35,14 @@ public abstract class Page extends Scene {
     public static final float DEFAULT_SCROLL_DELAY = 0.05f;
     public static final float NO_SCROLL_DELAY = 0;
 
+    public static final int MAX_OPTS_PER_SECT = 4;
+    public static final int FONT_SIZE = 20;
+
+    private static final Font font = new Font("Monospaced", Font.PLAIN, FONT_SIZE);
+    private static final Font info = new Font("Monospaced", Font.PLAIN, 8);
+
+    public final int lineBreakWidth;
+
     protected float time;
     protected float scrollDelay;
 
@@ -42,13 +50,51 @@ public abstract class Page extends Scene {
     protected int charProgress;
     protected int lineProgress;
 
-    protected abstract void updateOptions(String... options);
+    private int optSect;
+    private boolean renderSelector = true;
 
-    protected abstract void updateMessage(String message);
+    private String[] lines;
+    private String[] options;
+    private int[] optHeight;
+
+    protected Color bgColor = Color.black;
+    protected Color messageColor = Color.white;
+    protected Color optionsColor = Color.white;
+
+    protected Image image = null;
+
+    protected Page(int lineBreakWidth) {
+        this.lineBreakWidth = lineBreakWidth;
+    }
+
+    protected void updateOptions(String... options) {
+        this.options = options;
+        this.optHeight = new int[Math.min(MAX_OPTS_PER_SECT, options.length)];
+    }
+
+    protected void updateMessage(String message) {
+        this.lines = wrapText(message, lineBreakWidth);
+    }
 
     protected void updateScrollDelay(float newDelay) {
         this.scrollDelay = Math.max(newDelay, 0);
     }
+
+    protected int getLineCount() {
+        return lines.length;
+    }
+
+    protected int getOptionCount() {
+        return options.length;
+    }
+
+    protected abstract int calcImageX();
+
+    protected abstract int calcImageY();
+
+    protected abstract int calcMessageX();
+
+    protected abstract int calcMessageY();
 
     protected void scrollNextLine() {
         ++lineProgress;
@@ -62,9 +108,11 @@ public abstract class Page extends Scene {
 
     protected void resetOptionSelection() {
         option = 0;
+        optSect = 0;
     }
 
-    protected String[] wrapText(final String text, final int lineBreakWidth) {
+    private static String[] wrapText(final String text, final int lineBreakWidth) {
+        if (text == null) return new String[0];
         if (lineBreakWidth < 1) throw new IllegalArgumentException("br-width must >= 1, got " + lineBreakWidth);
 
         // break text down into lineBreakWidth char-limits lines.
@@ -89,13 +137,149 @@ public abstract class Page extends Scene {
         return list.toArray(new String[list.size()]);
     }
 
-    public abstract boolean doneScrolling();
+    public boolean doneScrolling() {
+        return lineProgress >= lines.length;
+    }
+
+    @Override
+    public void render(IGraphics g) {
+        g.setClearColor(bgColor);
+        g.clearGraphics();
+        if (image != null) g.drawImage(image, calcImageX(), calcImageY());
+
+        final int baseWidth = calcMessageX();
+        final int baseHeight = calcMessageY();
+
+        g.setFont(font);
+        if (charProgress > 0 || lineProgress > 0) {
+            // Idea is that if there are no options to render, the message can take up more space
+            final int bound = Math.min(lineProgress + 1, lines.length);
+            g.setColor(messageColor);
+            for (int i = 0; i < bound; ++i) {
+                final String s = lines[i];
+                if (s == null) {
+                    // Do not render <null>
+                    if (i < lineProgress) {
+                        continue;
+                    } else {
+                        scrollNextLine();
+                        break;
+                    }
+                }
+
+                if (i < lineProgress) {
+                    // Render full line, line was already scrolled through
+                    g.drawString(s, baseWidth + 20, baseHeight + i * FONT_SIZE);
+                } else if (i == lineProgress) {
+                    // Render partial line, line is currently being scrolled through
+                    boolean flag = false;
+                    int k = charProgress;
+                    if (k >= s.length()) {
+                        k = s.length();
+                        scrollNextLine();
+                        flag = true;
+                    }
+                    final String actualMessage = s.substring(0, k);
+                    g.drawString(actualMessage, baseWidth + 20, baseHeight + i * FONT_SIZE);
+
+                    if (flag) break;
+                }
+                // otherwise do not render, line will be scrolled through eventually
+            }
+
+            // Only render the option list if message was scrolled through entirely
+            if (doneScrolling()) {
+                final int newBase = baseHeight + (lines.length + 1) * FONT_SIZE;
+                final int lower = optSect * MAX_OPTS_PER_SECT;
+                final int optDispCount = Math.min(options.length - lower, MAX_OPTS_PER_SECT);
+                g.setColor(optionsColor);
+                for (int i = 0; i < optDispCount; ++i) {
+                    final int offset = i + lower;
+                    final int h = newBase + i * FONT_SIZE;
+                    final String s = options[offset];
+                    if (s == null) continue;    // Do not render <null>
+                    g.drawString(s, baseWidth + 40, h);
+                    optHeight[offset] = h - FONT_SIZE / 2 + 2;
+                }
+
+                if (renderSelector && option >= 0 && option < optHeight.length) {
+                    g.setColor(optionsColor);
+                    g.fillCircle(baseWidth + 30, optHeight[option], 5);
+                }
+
+                final int optSectCount = (options.length - 1) / MAX_OPTS_PER_SECT;
+                if (optSectCount > 0) {
+                    g.setColor(messageColor);
+                    g.setFont(info);
+                    g.drawString("Option Page (" + (optSect + 1) + "/" + (optSectCount + 1) + ")",
+                            baseWidth + 10, App.HEIGHT - 4);
+                }
+            }
+        }
+    }
+
+    private void normalizeOption() {
+        if (option < 0) {
+            option = optHeight.length - 1;
+        } else if (option >= optHeight.length) {
+            option = 0;
+        }
+
+        // Update optSect to display the correct list of options
+        optSect = option / MAX_OPTS_PER_SECT;
+    }
 
     @Override
     public boolean update(float dt) {
         if ((time += dt) >= scrollDelay) {
             ++charProgress;
             time -= scrollDelay;
+        }
+
+        if (options.length > 0) {
+            // Only contains -1, 0, +1
+            int delta = 0;
+
+            // Modify option by 1
+            if (scene.keyboard().isKeyPressed(KeyEvent.VK_UP)) delta = -1;
+            if (scene.keyboard().isKeyPressed(KeyEvent.VK_DOWN)) delta = +1;
+            option += delta;
+
+            // Modify option by a full optSect
+            if (scene.keyboard().isKeyPressed(KeyEvent.VK_LEFT)) {
+                option = (--optSect) * MAX_OPTS_PER_SECT;
+                delta = -1;
+            }
+            if (scene.keyboard().isKeyPressed(KeyEvent.VK_RIGHT)) {
+                option = (++optSect) * MAX_OPTS_PER_SECT;
+                delta = +1;
+            }
+
+            normalizeOption();
+
+            // guard against all null options which would have caused infinte loop
+            final int start = option;
+            while (options[option] == null) {
+                // Skip that index, it is non-selectable
+                option += delta;
+                normalizeOption();
+                if (start == option) {
+                    renderSelector = false;
+                    option = -1;
+                    break;
+                }
+            }
+        } else {
+            renderSelector = false;
+            option = -1;
+        }
+
+        if (scene.keyboard().isKeyPressed(KeyEvent.VK_ENTER)) {
+            if (doneScrolling()) {
+                return optionSelected(option);
+            } else {
+                scrollNextLine();
+            }
         }
         return true;
     }
--- Vcs-cycle/src/main/java/org/atoiks/games/vcsc/VerticalPage.java
+++ Vcs-cycle/src/main/java/org/atoiks/games/vcsc/VerticalPage.java
@@ -18,47 +18,18 @@
 
 package org.atoiks.games.vcsc;
 
-import java.awt.Font;
-import java.awt.Color;
-import java.awt.Image;
-
-import java.awt.event.KeyEvent;
-
-import java.util.List;
-import java.util.ArrayList;
-
-import org.atoiks.games.framework2d.IGraphics;
-
 public abstract class VerticalPage extends Page {
 
     public static final int LINE_BREAK_WIDTH = 25;
 
-    private static final int MAX_OPTS_PER_SECT = 4;
-    private static final int FONT_SIZE = 20;
-    private static final Font font = new Font("Monospaced", Font.PLAIN, FONT_SIZE);
-    private static final Font info = new Font("Monospaced", Font.PLAIN, 8);
-
-    private int optSect;
-    private boolean renderSelector = true;
-
-    private String[] lines;
-    private String[] options;
-    private int[] optHeight;
-
     protected boolean textOnRight = false;
-    protected Color messageColor = Color.white;
-    protected Color optionsColor = Color.white;
-    protected Image background = null;
 
     protected VerticalPage() {
-        this(DEFAULT_SCROLL_DELAY);
+        this(DEFAULT_SCROLL_DELAY, null);
     }
 
     protected VerticalPage(float scrollDelay) {
-        this.lines = new String[0];
-        this.options = new String[0];
-        this.optHeight = new int[0];
-        updateScrollDelay(scrollDelay);
+        this(scrollDelay, null);
     }
 
     public VerticalPage(final String message, final String... options) {
@@ -66,173 +37,29 @@ public abstract class VerticalPage extends Page {
     }
 
     public VerticalPage(float scrollDelay, String message, String... options) {
+        super(LINE_BREAK_WIDTH);
         updateMessage(message);
         updateOptions(options);
         updateScrollDelay(scrollDelay);
     }
 
     @Override
-    protected final void updateOptions(String... options) {
-        this.options = options;
-        this.optHeight = new int[options.length];
-    }
-
-    @Override
-    protected final void updateMessage(final String message) {
-        this.lines = wrapText(message, LINE_BREAK_WIDTH);        
-    }
-
-    private int getMessageRenderX() {
+    protected int calcMessageX() {
         return (textOnRight ? App.WIDTH / 2 : 0) + 20;
     }
 
     @Override
-    public void render(IGraphics g) {
-        g.setClearColor(Color.black);
-        g.clearGraphics();
-        if (background != null) g.drawImage(background, textOnRight ? 0 : App.WIDTH / 2, 0);
-
-        g.setFont(font);
-        if (charProgress > 0 || lineProgress > 0) {
-            // Idea is that if there are no options to render, the message can take up more space
-            final int baseHeight = 3 * App.HEIGHT / 4 -
-                    Math.max(lines.length - (optHeight.length == 0 ? 3 : 1), 1) * FONT_SIZE;
-            final int bound = Math.min(lineProgress + 1, lines.length);
-            g.setColor(messageColor);
-            for (int i = 0; i < bound; ++i) {
-                final String s = lines[i];
-                if (s == null) {
-                    // Do not render <null>
-                    if (i < lineProgress) {
-                        continue;
-                    } else {
-                        scrollNextLine();
-                        break;
-                    }
-                }
-
-                if (i < lineProgress) {
-                    // Render full line, line was already scrolled through
-                    g.drawString(s, getMessageRenderX(), baseHeight + i * FONT_SIZE);
-                } else if (i == lineProgress) {
-                    // Render partial line, line is currently being scrolled through
-                    boolean flag = false;
-                    int k = charProgress;
-                    if (k >= s.length()) {
-                        k = s.length();
-                        scrollNextLine();
-                        flag = true;
-                    }
-                    final String actualMessage = s.substring(0, k);
-                    g.drawString(actualMessage, getMessageRenderX(), baseHeight + i * FONT_SIZE);
-
-                    if (flag) break;
-                }
-                // otherwise do not render, line will be scrolled through eventually
-            }
-
-            // Only render the option list if message was scrolled through entirely
-            if (doneScrolling()) {
-                final int newBase = baseHeight + (lines.length + 1) * FONT_SIZE;
-                final int lower = optSect * MAX_OPTS_PER_SECT;
-                final int optDispCount = Math.min(options.length - lower, MAX_OPTS_PER_SECT);
-                g.setColor(optionsColor);
-                for (int i = 0; i < optDispCount; ++i) {
-                    final int offset = i + lower;
-                    final int h = newBase + i * FONT_SIZE;
-                    final String s = options[offset];
-                    if (s == null) continue;    // Do not render <null>
-                    g.drawString(s, getMessageRenderX() + 20, h);
-                    optHeight[offset] = h - FONT_SIZE / 2 + 2;
-                }
-
-                if (renderSelector && option >= 0 && option < optHeight.length) {
-                    g.setColor(optionsColor);
-                    g.fillCircle(getMessageRenderX() + 10, optHeight[option], 5);
-                }
-
-                final int optSectCount = (options.length - 1) / MAX_OPTS_PER_SECT;
-                if (optSectCount > 0) {
-                    g.setColor(messageColor);
-                    g.setFont(info);
-                    g.drawString("Option Page (" + (optSect + 1) + "/" + (optSectCount + 1) + ")",
-                            getMessageRenderX() - 10, App.HEIGHT - 4);
-                }
-            }
-        }
-    }
-
-    private void normalizeOption() {
-        if (option < 0) {
-            option = optHeight.length - 1;
-        } else if (option >= optHeight.length) {
-            option = 0;
-        }
-
-        // Update optSect to display the correct list of options
-        optSect = option / MAX_OPTS_PER_SECT;
+    protected int calcMessageY() {
+        return 3 * App.HEIGHT / 4 - Math.max(getLineCount() - (getOptionCount() == 0 ? 3 : 1), 1) * FONT_SIZE;
     }
 
     @Override
-    protected void resetOptionSelection() {
-        super.resetOptionSelection();
-        optSect = 0;
+    protected int calcImageX() {
+        return textOnRight ? 0 : App.WIDTH / 2;
     }
 
     @Override
-    public boolean doneScrolling() {
-        return lineProgress >= lines.length;
-    }
-
-    @Override
-    public boolean update(float dt) {
-        super.update(dt);
-
-        if (options.length > 0) {
-            // Only contains -1, 0, +1
-            int delta = 0;
-
-            // Modify option by 1
-            if (scene.keyboard().isKeyPressed(KeyEvent.VK_UP)) delta = -1;
-            if (scene.keyboard().isKeyPressed(KeyEvent.VK_DOWN)) delta = +1;
-            option += delta;
-
-            // Modify option by a full optSect
-            if (scene.keyboard().isKeyPressed(KeyEvent.VK_LEFT)) {
-                option = (--optSect) * MAX_OPTS_PER_SECT;
-                delta = -1;
-            }
-            if (scene.keyboard().isKeyPressed(KeyEvent.VK_RIGHT)) {
-                option = (++optSect) * MAX_OPTS_PER_SECT;
-                delta = +1;
-            }
-
-            normalizeOption();
-
-            // guard against all null options which would have caused infinte loop
-            final int start = option;
-            while (options[option] == null) {
-                // Skip that index, it is non-selectable
-                option += delta;
-                normalizeOption();
-                if (start == option) {
-                    renderSelector = false;
-                    option = -1;
-                    break;
-                }
-            }
-        } else {
-            renderSelector = false;
-            option = -1;
-        }
-
-        if (scene.keyboard().isKeyPressed(KeyEvent.VK_ENTER)) {
-            if (doneScrolling()) {
-                return optionSelected(option);
-            } else {
-                scrollNextLine();
-            }
-        }
-        return true;
+    protected int calcImageY() {
+        return 0;
     }
 }
